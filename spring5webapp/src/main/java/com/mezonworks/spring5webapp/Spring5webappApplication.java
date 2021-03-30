6c3445dd85a8f40c232195c7b2443ceef302accd
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-03-30 15:54:53 +0900

    initial commit

---
+++ spring5webapp/src/main/java/com/mezonworks/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,13 @@
+package com.mezonworks.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+
+}
