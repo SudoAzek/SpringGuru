9203778dbf2cb3ae88909f2ed5aa39122e26b234
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-04-06 17:57:48 +0900

    initial commit for dependency injection

---
+++ spring5di/src/main/java/com/mezonworks/spring5di/Spring5diApplication.java
@@ -0,0 +1,13 @@
+package com.mezonworks.spring5di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5diApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5diApplication.class, args);
+    }
+
+}
---
+++ spring5di/src/test/java/com/mezonworks/spring5di/Spring5diApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mezonworks.spring5di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5diApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
