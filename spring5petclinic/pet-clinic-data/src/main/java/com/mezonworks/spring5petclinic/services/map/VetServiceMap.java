37df2d0ec81dac24a6e2db06aab556377cb50bf4
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-04-13 13:58:56 +0900

    Implement Map Based Services
    Closes #9

---
+++ spring5petclinic/pet-clinic-data/src/main/java/com/mezonworks/spring5petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.mezonworks.spring5petclinic.services.map;
+
+import com.mezonworks.spring5petclinic.model.Owner;
+import com.mezonworks.spring5petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Long id,Owner object) {
+        return super.save(id, object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
